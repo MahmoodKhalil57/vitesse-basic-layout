9497832cf560b71f0e914275373c7670be7a3316
MahmoodKhalil57 <2393401k@student.gla.ac.uk>
2022-10-15 00:15:54 +0100

    autoimports

--- src/components.d.ts
+++ src/components.d.ts
@@ -8,9 +8,12 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Counter: typeof import('./components/Counter.vue')['default']
-    Footer: typeof import('./components/Footer.vue')['default']
+    Footer: typeof import('./components/layout/Footer.vue')['default']
+    Header: typeof import('./components/layout/Header.vue')['default']
+    Logo: typeof import('./components/brand/Logo.vue')['default']
     README: typeof import('./components/README.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SideMenu: typeof import('./components/layout/SideMenu.vue')['default']
   }
 }
