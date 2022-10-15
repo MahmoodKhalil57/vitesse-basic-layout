b1e6f1d5b41f79efba0d236491f3cbaf074ec9ea
MahmoodKhalil57 <2393401k@student.gla.ac.uk>
2022-10-15 22:25:01 +0100

    add extra section to index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,8 +12,8 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <div flex flex-col h-full items-center>
-    <div flex flex-col items-center>
+  <div flex flex-col h-full w-full items-center>
+    <div flex flex-col items-center justify-center h-screen w-full bg-dark-800 p-5>
       <div text-4xl>
         <div i-fa6-solid-paint-roller inline-block />
       </div>
@@ -60,101 +60,7 @@ const { t } = useI18n()
         </button>
       </div>
     </div>
-    <div flex flex-col items-center>
-      <div text-4xl>
-        <div i-fa6-solid-paint-roller inline-block />
-      </div>
-      <p>
-        <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-          [Vitesse]
-        </a>
-      </p>
-      <p>
-        <a rel="noreferrer" href="https://github.com/MahmoodKhalil57/vitesse-basic-layout" target="_blank">
-          [Vitesse Basic Layout]
-        </a>
-      </p>
-      <p>
-        <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-      </p>
-
-      <div py-4 />
-
-      <input
-        id="input"
-        v-model="name"
-        :placeholder="t('intro.whats-your-name')"
-        :aria-label="t('intro.whats-your-name')"
-        type="text"
-        autocomplete="false"
-        p="x4 y2"
-        w="250px"
-        text="center"
-        bg="transparent"
-        border="~ rounded gray-200 dark:gray-700"
-        outline="none active:none"
-        @keydown.enter="go"
-      >
-      <label hidden for="input">{{ t('intro.whats-your-name') }}</label>
-
-      <div>
-        <button
-          btn m-3 text-sm
-          :disabled="!name"
-          @click="go"
-        >
-          {{ t('button.go') }}
-        </button>
-      </div>
-    </div>
-    <div flex flex-col items-center>
-      <div text-4xl>
-        <div i-fa6-solid-paint-roller inline-block />
-      </div>
-      <p>
-        <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-          [Vitesse]
-        </a>
-      </p>
-      <p>
-        <a rel="noreferrer" href="https://github.com/MahmoodKhalil57/vitesse-basic-layout" target="_blank">
-          [Vitesse Basic Layout]
-        </a>
-      </p>
-      <p>
-        <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-      </p>
-
-      <div py-4 />
-
-      <input
-        id="input"
-        v-model="name"
-        :placeholder="t('intro.whats-your-name')"
-        :aria-label="t('intro.whats-your-name')"
-        type="text"
-        autocomplete="false"
-        p="x4 y2"
-        w="250px"
-        text="center"
-        bg="transparent"
-        border="~ rounded gray-200 dark:gray-700"
-        outline="none active:none"
-        @keydown.enter="go"
-      >
-      <label hidden for="input">{{ t('intro.whats-your-name') }}</label>
-
-      <div>
-        <button
-          btn m-3 text-sm
-          :disabled="!name"
-          @click="go"
-        >
-          {{ t('button.go') }}
-        </button>
-      </div>
-    </div>
-    <div flex flex-col items-center>
+    <div flex flex-col items-center justify-center h-100 w-full bg-dark-1000 p-5>
       <div text-4xl>
         <div i-fa6-solid-paint-roller inline-block />
       </div>
