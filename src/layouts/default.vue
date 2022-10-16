ec5d5ec10d80a220ba5659af2946d671907faeca
MahmoodKhalil57 <2393401k@student.gla.ac.uk>
2022-10-16 15:46:33 +0100

    cleanup text theming

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -15,7 +15,7 @@ const isHome = computed(() => String(route.path) === '/')
   <main flex flex-col justify-between min-h-screen>
     <div v-if="isHome" z-1 fixed w-full class="-mt-3">
       <HeaderFadingBackdrop @emit-scrolled="handleScrolled" />
-      <Header :class="{ 'text-white': scrolled }" @emit-open="handleOpenDrawer" />
+      <Header :class="{ 'c-text-2': scrolled }" @emit-open="handleOpenDrawer" />
     </div>
 
     <div v-else z-1 class="c-sticky -mt-3">
@@ -27,8 +27,8 @@ const isHome = computed(() => String(route.path) === '/')
       <RouterView lt-sm="max-w-full" />
     </div>
 
-    <Footer text-white class="c-lo-1" />
-    <SideMenu v-model="drawerOpen" z-2 />
+    <Footer class="c-lo-1 c-text-2" />
+    <SideMenu v-model="drawerOpen" z-2 class=" c-text-2" />
   </main>
 </template>
 
